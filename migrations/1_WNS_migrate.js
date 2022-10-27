65937ca933c37a7914c203b857a47c966cb9f6b8
ichristwin <ichristwin@gmail.com>
2022-10-27 11:41:47 +0100

    new migrations script & dependencies

--- migrations/1_WNS_migrate.js
+++ migrations/1_WNS_migrate.js
@@ -2,6 +2,6 @@ const WNS = artifacts.require("WNS")
 
 module.exports = async function (deployer) {
     await deployer.deploy(WNS);
-    const wns = await WNS.deployed();
-    console.log("deployed at", wns.address);
+    const instance = await WNS.deployed();
+    console.log("deployed at", instance.address);
 }
--- migrations/2_WNSMarketplace_migrate.js
+++ migrations/2_WNSMarketplace_migrate.js
@@ -1,7 +1,21 @@
+// migrations/NN_deploy_upgradeable_box.js
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const Marketplace = artifacts.require("Marketplace")
 
 module.exports = async function (deployer) {
-    await deployer.deploy(Marketplace);
-    const marketplace = await Marketplace.deployed();
-    console.log("deployed at", marketplace.address);
-}
+  const instance = await deployProxy(Marketplace, [42], { deployer });
+  console.log('Deployed at', instance.address);
+};
+
+
+// // migrations/MM_upgrade_box_contract.js
+// const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+// const Box = artifacts.require('Box');
+// const BoxV2 = artifacts.require('BoxV2');
+
+// module.exports = async function (deployer) {
+//   const existing = await Box.deployed();
+//   const instance = await upgradeProxy(existing.address, BoxV2, { deployer });
+//   console.log("Upgraded", instance.address);
+// };
